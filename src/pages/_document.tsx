8b0ce875f37035a0de01a2b518c5904ce65bab04
ArekRGW <arkadiusz.pawlak.web@gmail.com>
2022-04-02 10:02:58 +0200

    added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 			<Html lang="en">
 				<Head>
 					{/* PWA primary color */}
-					<link rel="shortcut icon" href="/static/favicon.ico" />
+					<link rel="shortcut icon" href="/favicon.png" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link
 						rel="preconnect"
